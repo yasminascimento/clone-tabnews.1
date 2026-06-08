05631873e7234b1751a3d13fd4edd2ca9829bf9e
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2026-06-08 11:24:19 +0000

    adiciona scripts `lint:check `e `lint:fix `

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews.1
+
 Projeto curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>yasmin!!2</h1>
-};
+function Home() {
+  return <h1>yasmin!!2</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
+}
 
 export default Home;
